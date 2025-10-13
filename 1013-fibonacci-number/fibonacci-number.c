4b586d16c918ae23bd74246bd6333ad040411046
Shrilakshmi <120452381+ShriBangera@users.noreply.github.com>
2025-10-13 20:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.23%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.c
@@ -0,0 +1,13 @@
+int fib(int n){
+    int i,t1=0,t2=1,res;
+    if(n==0)
+        return t1;
+    if(n==1)
+        return t2;
+    for(i=2;i<=n;i++){
+        res=t1+t2;
+        t1=t2;
+        t2=res;
+    }
+    return res;
+}
